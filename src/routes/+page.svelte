698cb79adff3ba9f09bb94b6b5c5d27f9e15cbf0
aw <aw@a0.ax>
2025-06-03 12:28:19 -0500

    remove footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,9 @@
 <script lang="ts">
-	import Nav from "$lib/components/nav/nav.svelte";
 	import MapSection from "$lib/components/map/map-section.svelte";
-	import Footer from "$lib/components/footer.svelte";
 </script>
 
 <div class="min-h-dvh bg-background text-foreground">
-	<Nav />
 	<main class="md:mx-16">
 		<MapSection />
 	</main>
-	<Footer />
 </div>
